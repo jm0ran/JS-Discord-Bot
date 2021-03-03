bc8ab07111b4df51c19f8c9945c3cbd0dea03d35
jm0ran <josephdeargmoran@gmail.com>
2021-03-02 20:28:06 -0500

    Basic Startup + Connection

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-var userSpecific = require("./userSpecific");
-console.log(userSpecific.message);
+const userSpecific = require("./userSpecific"); //Imports userSpecific, this is where you import userSpecific data such as your bot's token
+const Discord = require("discord.js"); //Imports DiscordJS
+
+const client = new Discord.Client(); //Creates a new client/bot
+
+client.once('ready', function(){ //Runs once on client startup
+    console.log("Start Up Initiated"); //Logs an initialization message
+})
+
+
+client.login(userSpecific.token); //Logs into client using token from the userSpecific
